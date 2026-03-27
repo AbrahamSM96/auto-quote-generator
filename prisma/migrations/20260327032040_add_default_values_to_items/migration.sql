05e7595e81aac7bc10a49e542f5e26f7848b8a72
Abraham Serrano <abrahamserrano96@hotmail.com>
2026-03-26 23:47:41 -0600

    feat: set default values for JSON fields in Quotation model and update .gitignore for generated files

---
+++ prisma/migrations/20260327032040_add_default_values_to_items/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Quotation" ALTER COLUMN "bodyworkItems" SET DEFAULT '[]',
+ALTER COLUMN "paintItems" SET DEFAULT '[]',
+ALTER COLUMN "partItems" SET DEFAULT '[]',
+ALTER COLUMN "mechanicalItems" SET DEFAULT '[]';
